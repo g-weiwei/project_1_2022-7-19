a6128680fd264e0c665ba4e2949066a555d5ffb6
weiwei <g_weiwei@foxmail.com>
2022-07-19 21:07:14 +0800

    第一次提交

---
+++ server.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var url = require("url");
+var fs = require("fs");
+
+http
+  .createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "dome.html" });
+        return res.end("11111111");
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
